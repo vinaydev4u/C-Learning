80d8ba37cb0d5b82f0de95cd176f7ee2f31dd87d
vinaydevmd <vinaydev4u@gmail.com>
2019-10-17 16:17:20 +0530

    LINQ and Docker containerization application

---
+++ LINQLearning/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQLearning
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IEnumerable<string> cities = new[] {"Ghent","London","Las Vegas","Hydebad"};
+
+            IEnumerable<string> query = 
+            cities.StringThatStartWith("L");
+        }
+    }
+}
+
+namespace Extensions
+{
+    public static class FilterExtensions
+    {
+        public static IEnumerable<string> StringsThatStartWith
+        (this IEnumerable<string> input,string start)
+        {
+            
+        }
+    }
+}
